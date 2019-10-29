9075d47b6db6383a71d1c57276afdd65a8f2dd97
Alexandre Gamelas <you@example.com>
2019-10-29 12:39:39 +0100

    app

---
+++ app.php
@@ -0,0 +1,17 @@
+<?php include_once "componentes/head.php" ?>
+<body class="bg-light-grey">
+    <main class="container-fluid">
+       <?php include_once "views/distribution.php" ?>
+       <?php include_once "views/week.php" ?>
+       <?php include_once "views/budget.php" ?>
+       <?php include_once "views/profile.php" ?>
+       <?php include_once "componentes/modais.php" ?>
+    </main>
+
+    <footer>
+        <?php include_once "componentes/bot_menu.php" ?>
+    </footer>
+    <?php include_once "componentes/side_menu.php"?>
+    <script src="js/bot_menu.js"></script>
+</body>
+</html>
